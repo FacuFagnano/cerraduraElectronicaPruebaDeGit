b1fed84bfd7b585505f3ff11b215db3183d40a25
Facundo Damián Fagnano <facundofagnano@gmail.com>
2021-10-05 21:01:17 -0300

    facu

---
+++ src/cerraduraElectronica/CerraduraElectronica.java
@@ -0,0 +1,8 @@
+package cerraduraElectronica;
+
+public class CerraduraElectronica {
+	
+	public Boolean estaAbierta() {
+		return true;
+	}
+}
---
+++ test/cerraduraElectronica/CerraduraElectronicaTest.java
@@ -0,0 +1,14 @@
+package cerraduraElectronica;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CerraduraElectronicaTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
